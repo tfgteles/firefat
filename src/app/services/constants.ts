26b676abb066200dd026379d937be8769d0e47c2
Tiago Teles <tfgteles@gmail.com>
2021-10-01 07:08:26 -0400

    Base URL updated from http to https

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,5 +1,5 @@
 // export const BASE_URL: string = 'https://localhost:44319/';
-export const BASE_URL: string = 'http://firefatcapstone.azurewebsites.net/';
+export const BASE_URL: string = 'https://firefatcapstone.azurewebsites.net/';
 
 export const LOGIN_PATH: string = 'api/authmanagement/login';
 
